056a0e53d31c324466ae08f14cb0f1244b401257
Katya Pinkh <katya86.pin@gmail.com>
2022-01-24 23:43:39 +0200

    Subprocess module added and simple command checked

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+import subprocess
+
+
+def run_command(cmd):
+    result = subprocess.run(cmd, check=False, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return result.stdout.decode()
+
 
 def main():
-    print('Hi')
+    result = run_command('dir')
+    print(result)
+
 
 if __name__ == '__main__':
     main()
